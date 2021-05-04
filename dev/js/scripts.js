0cb3a62453b604bab2913f8a7a0ee72fba3cf5fa
Keelty <keeltydare@gmail.com>
2021-05-04 10:55:42 -0400

    animated stars

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -30,19 +30,20 @@ ready(() => {
 
   /* add your code here */
   let mainTL = gsap.timeline({id:"main"});
+  let lightTL = gsap.timeline({paused:true});
 
 
   function init(){
     //***********  starsTL init ****************
-gsap.set([".star-01", ".star-02", ".star-03", ".star-04", ".star-05", ".star-06", ".star-07", ".star-08"], {alpha:0})
-    //*********** asteroidsTL init ****************
-gsap.set(["#asteroid-1", "#asteroid-2", "#asteroid-3", "#asteroid-4", "#asteroid-5", "#asteroid-6", "#asteroid-7", "#asteroid-8", "#asteroid-9", "#asteroid-10"], {display:"none"})
+gsap.set([".star-01", ".star-02", ".star-03", ".star-04", ".star-05", ".star-06", ".star-07", ".star-08"], {alpha:0, borderRadius:"100%", rotation:"180deg", transformOrigin:"center center", scale:".25"});
     //*********** serenityTL init ****************
-gsap.set(["#serenity"], {display:"none"} )
+gsap.set(["#serenity"], {display:"none"});
+    //*********** asteroidsTL init ****************
+    gsap.set(["#asteroid-1", "#asteroid-2", "#asteroid-3", "#asteroid-4", "#asteroid-5", "#asteroid-6", "#asteroid-7", "#asteroid-8", "#asteroid-9", "#asteroid-10"], {display:"none"});
     //*********** asteroidDodgeTL init ****************
 
     //*********** flightTL init ****************
-gsap.set(["#symbol"], {display:"none"})
+gsap.set(["#symbol"], {display:"none"});
     //*********** bigSymbolTL init ****************
 
 
@@ -51,38 +52,67 @@ gsap.set(["#symbol"], {display:"none"})
   //Nested Timelines
   //***********  starsTL init  ****************
   function starsTL(){
+    let tl = gsap.timeline();
 
+    tl.to(".star-01", {alpha:1, duration:1, rotation:"-180deg", borderRadius:"none", scale:"1", ease:"power1.out"})
+      .to(".star-02", {alpha:1, duration:1, rotation:"-180deg", borderRadius:"none", scale:"1", ease:"power1.out"}, "-=.5")
+      .to(".star-03", {alpha:1, duration:1, rotation:"-180deg", borderRadius:"none", scale:"1", ease:"power1.out"}, "-=.5")
+      .to(".star-04", {alpha:1, duration:1, rotation:"-180deg", borderRadius:"none", scale:"1", ease:"power1.out"}, "-=.5")
+      .to(".star-05", {alpha:1, duration:1, rotation:"-180deg", borderRadius:"none", scale:"1", ease:"power1.out"}, "-=.5")
+      .to(".star-06", {alpha:1, duration:1, rotation:"-180deg", borderRadius:"none", scale:"1", ease:"power1.out"}, "-=.5")
+      .to(".star-07", {alpha:1, duration:1, rotation:"-180deg", borderRadius:"none", scale:"1", ease:"power1.out"}, "-=.5")
+      .to(".star-08", {alpha:1, duration:1, rotation:"-180deg", borderRadius:"none", scale:"1", ease:"power1.out"}, "-=.5")
+
+      ;
+    return tl;
+  }
+
+  //*********** serenityTL init ****************
+  function serenityTL(){
+    let tl = gsap.timeline();
+
+    tl.to("#serenity", {display:"block", onStart:light})
+
+    ;
+
+    return tl;
   }
 
   //*********** asteroidsTL init ****************
   function asteroidsTL(){
+    let tl = gsap.timeline();
 
+    return tl;
   }
-  //*********** serenityTL init ****************
-  function serenityTL(){
 
-  }
   //*********** asteroidDodgeTL init ****************
   function asteroidDodgeTL(){
+    let tl = gsap.timeline();
 
+    return tl;
   }
   //*********** flightTL ****************
   function flightTL(){
+    let tl = gsap.timeline();
 
+    return tl;
   }
   //*********** bigSymbolTL init ****************
   function bigSymbolTL(){
+    let tl = gsap.timeline();
 
+    return tl;
   }
 
   //*********** flame functions DO NOT INCLUDE IN MAIN TL ****************
   function light(){
   
     console.log('SHOW TIME');
-    gsap.set(".light",{display:"block", alpha:".5"});
+    gsap.set("#light",{display:"block", alpha:".1",transformOrigin:"center center"});
   
-    lightTL.to("#left-blue",{duration:0.25, scale:.75, aplha:".25", yoyo:true, repeat:-1},"flames")
-  
+    lightTL.to("#light",{duration:0.5, scale:.9, aplha:".25", yoyo:true, repeat:-1, ease:"power4.out"});
+    
+    lightTL.play()
   }
 
   //1. set initial properties
@@ -93,9 +123,14 @@ gsap.set(["#symbol"], {display:"none"})
 
   //3. BUILD Main timeline
 //  mainTL.add(fadeInTL())
+mainTL.add(starsTL())
+      .add(serenityTL())
+      .add(asteroidsTL())
+      .add(asteroidDodgeTL())
+      .add(flightTL())
+      .add(bigSymbolTL())
 
-
-  //;//tl END
+  ;//tl END
 
 
 
